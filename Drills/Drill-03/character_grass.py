0541658177dacb706d07a2e052921f866da96191
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-09-11 21:54:21 +0900

    one grass render
    second commit(and first commit at aorus)

--- Drills/Drill-03/character_grass.py
+++ Drills/Drill-03/character_grass.py
@@ -9,8 +9,7 @@ character = load_image('character.png')
 x = 0
 while ( x < 800) :
     clear_canvas_now()
-    for y in range(1, 3) :
-        grass.draw_now(400, 20*y + 10)
+    grass.draw_now(400, 30)
     character.draw_now(x, 90)
     x = x + 2
     delay(0.01)
