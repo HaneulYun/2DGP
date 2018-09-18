f325e80797c2d9a7b1d104f42da3ef8ba564a100
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-09-18 18:58:24 +0900

    complete function
    complete draw(x, y) function

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -6,6 +6,15 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
  # fill here
+
+def draw(x, y):
+    clear_canvas()
+    grass.draw(400, 30)
+    character.draw(x, y)
+    update_canvas()
+    delay(0.01)
+    pass
+
 def move(x1, y1, x2, y2):
     pass
 
