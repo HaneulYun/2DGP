5d1e154ef21858e14ba9ae8c771af68d79ac0360
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-09-20 19:54:11 +0900

    build structure
    make virtual function
     - handle_events
     - draw
     - move

--- Drills/Drill-06/DRill-06.py
+++ Drills/Drill-06/DRill-06.py
@@ -7,6 +7,20 @@ open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
+def handle_events():
+    pass
+
+def draw(x, y):
+    pass
+
+def move():
+    pass
+
+running = True
+x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+mouseX, mouseY = x, y
+frame = 0
+
 while True:
     pass
 
