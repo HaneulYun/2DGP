739efa2c93123a6ec78e5eed5f3cf8e319d4bf1e
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-09-20 20:36:05 +0900

    complete Drill-06
    2017182027 윤하늘
    Drill-06 complete / 7 commit
    https://www.youtube.com/watch?v=67wV1KwIrPA&feature=youtu.be

--- Drills/Drill-06/DRill-06.py
+++ Drills/Drill-06/DRill-06.py
@@ -4,7 +4,6 @@ import  math
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
-
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 cursor = load_image('hand_arrow.png')
