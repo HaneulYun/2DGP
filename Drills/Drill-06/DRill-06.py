43a10350ac4026f5bc4ebf0135862595c38c0bb8
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-09-20 20:31:07 +0900

    complete Drill-06

--- Drills/Drill-06/DRill-06.py
+++ Drills/Drill-06/DRill-06.py
@@ -11,7 +11,7 @@ cursor = load_image('hand_arrow.png')
 
 def handle_events():
     global mouseX, mouseY, targetX, targetY, x, y
-    global distance
+    global distance, direction
     global running, moving
     events = get_events()
     for event in events:
@@ -23,15 +23,22 @@ def handle_events():
             moving = True
             distance = math.sqrt((mouseX - x)**2 + (mouseY-y)**2)
             targetX, targetY = mouseX, mouseY
+            if targetX > x:
+                direction = True
+            else:
+                direction = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
 def draw(x, y):
     global frame
+    action = (1-moving) * 2
+    if(targetX >= x):
+        action += direction
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    character.clip_draw(frame * 100, 100 * action, 100, 100, x, y)
     frame = (frame + 1) % 8
-    cursor.draw(mouseX, mouseY)
+    cursor.draw(mouseX + cursor.w // 2, mouseY - cursor.h // 2)
     update_canvas()
 
 def move():
@@ -40,18 +47,20 @@ def move():
         radian = math.atan2(targetY - y, targetX - x)
         x, y = x + math.cos(radian) * 10, y + math.sin(radian) * 10
         distance = math.sqrt((targetX - x) ** 2 + (targetY - y) ** 2)
-        if distance < 4:
+        if distance < 6:
             x, y = targetX, targetY
             moving = False
     draw(x, y)
 
 running = True
 moving = False
+direction = True
 distance = 0.0
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 mouseX, mouseY = x, y
 targetX, targetY = x, y
 frame = 0
+hide_cursor()
 
 while running:
     handle_events()
