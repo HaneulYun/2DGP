189a427c2a6abf21a77b590221721f5cc4fbf9c8
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-09-20 18:36:20 +0900

    make structure
    make virtual draw function
    make virtual move function
    make virtual input function

--- Drills/Drill-06/DRill-06.py
+++ Drills/Drill-06/DRill-06.py
@@ -1,3 +1,15 @@
 from pico2d import *
 
-# fill here
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
+
+def draw()
+    pass
+
+def move()
+    pass
+
+def input()
+    pass
+
+while True:
