4f2f01e1a76e2ce961f0f78d0a25115d4d356d1b
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-09-20 18:54:58 +0900

    making draw
    checking commit

--- Drills/Drill-06/DRill-06.py
+++ Drills/Drill-06/DRill-06.py
@@ -1,15 +1,25 @@
 from pico2d import *
 
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
-def draw()
+def draw(radian, x, y) :
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.rotate_draw(radian, x, y)
+    update_canvas()
+    delay(0.1)
+
+def move(x1, y1, x2, y2) :
     pass
 
-def move()
-    pass
-
-def input()
+def input() :
     pass
 
 while True:
+    draw(0, KPU_WIDTH // 2, KPU_HEIGHT // 2)
+
+close_canvas()
