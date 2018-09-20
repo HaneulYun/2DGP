67926b6e038cfebc4e6928ba450e8d081e8eb749
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-09-20 20:19:32 +0900

    complete function
    complete function
     - move

--- Drills/Drill-06/DRill-06.py
+++ Drills/Drill-06/DRill-06.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import  math
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
@@ -9,9 +10,9 @@ character = load_image('animation_sheet.png')
 cursor = load_image('hand_arrow.png')
 
 def handle_events():
-    global mouseX, mouseY
-    global moving
-    global running
+    global mouseX, mouseY, targetX, targetY, x, y
+    global distance
+    global running, moving
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -20,6 +21,8 @@ def handle_events():
             mouseX, mouseY = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
             moving = True
+            distance = math.sqrt((mouseX - x)**2 + (mouseY-y)**2)
+            targetX, targetY = mouseX, mouseY
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
@@ -32,15 +35,27 @@ def draw(x, y):
     update_canvas()
 
 def move():
-    pass
+    global x, y, distance, moving
+    if moving:
+        radian = math.atan2(targetY - y, targetX - x)
+        x, y = x + math.cos(radian) * 10, y + math.sin(radian) * 10
+        distance = math.sqrt((targetX - x) ** 2 + (targetY - y) ** 2)
+        if distance < 4:
+            x, y = targetX, targetY
+            moving = False
+    draw(x, y)
 
 running = True
 moving = False
+distance = 0.0
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 mouseX, mouseY = x, y
+targetX, targetY = x, y
 frame = 0
 
 while running:
-    pass
+    handle_events()
+    move()
+    delay(1/60)
 
 close_canvas()
