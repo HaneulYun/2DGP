1e72c0af05a834e64ec0b9f25f413cf0626d88a6
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-09-20 18:57:11 +0900

    make structure
    make virtual handle_event function

--- Drills/Drill-06/DRill-06.py
+++ Drills/Drill-06/DRill-06.py
@@ -6,18 +6,21 @@ open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
-def draw(radian, x, y) :
+def handle_event():
+
+def draw(radian, x, y):
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     character.rotate_draw(radian, x, y)
     update_canvas()
     delay(0.1)
 
-def move(x1, y1, x2, y2) :
+def move(x1, y1, x2, y2):
     pass
 
-def input() :
+def input():
     pass
+
 
 while True:
     draw(0, KPU_WIDTH // 2, KPU_HEIGHT // 2)
