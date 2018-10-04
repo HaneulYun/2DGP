3be7bba1efab351350e6bdebf51fade91f89897b
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-10-04 18:41:57 +0900

    making Drill-08-2
    making Drill-08-2

--- Drills/Drill-08/Drill-08-2.py
+++ Drills/Drill-08/Drill-08-2.py
@@ -43,13 +43,18 @@ def draw(x, y):
 def move():
     global x, y, distance, moving
     if moving:
-        radian = math.atan2(targetY - y, targetX - x)
-        x, y = x + math.cos(radian) * 10, y + math.sin(radian) * 10
-        distance = math.sqrt((targetX - x) ** 2 + (targetY - y) ** 2)
         if distance < 6:
             x, y = targetX, targetY
             moving = False
     draw(x, y)
+
+def set_curve_4_points(p1, p2, p3, p4, ratio):
+    global x, y
+    t = ratio / 100
+    x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
+                        -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+    y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
+                        -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
 
 running = True
 moving = False
@@ -60,6 +65,7 @@ mouseX, mouseY = x, y
 targetX, targetY = x, y
 frame = 0
 hide_cursor()
+t = 0
 
 import random
 
