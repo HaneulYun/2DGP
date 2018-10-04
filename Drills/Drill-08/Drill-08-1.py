de2882bd5da36e0655c69f88acb6ff59d78bfb56
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-10-04 18:25:29 +0900

    making Drill-08-1
    making Drill-08-1

--- Drills/Drill-08/Drill-08-1.py
+++ Drills/Drill-08/Drill-08-1.py
@@ -73,12 +73,12 @@ def draw_curve_4_points(p1, p2, p3, p4):
     draw_big_point(p4)
 
     # draw p1-p2
-    for i in range(0, 50, 2):
-        t = i / 100
-        x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
-        y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
-        draw_point((x, y))
-    draw_point(p2)
+    #for i in range(0, 50, 2):
+    #    t = i / 100
+    #    x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
+    #    y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
+    #    draw_point((x, y))
+    #draw_point(p2)
 
     # draw p2-p3
     for i in range(0, 100, 2):
@@ -89,16 +89,20 @@ def draw_curve_4_points(p1, p2, p3, p4):
     draw_point(p3)
 
     # draw p3-p4
-    for i in range(50, 100, 2):
-        t = i / 100
-        x = (2*t**2-3*t+1)*p2[0]+(-4*t**2+4*t)*p3[0]+(2*t**2-t)*p4[0]
-        y = (2*t**2-3*t+1)*p2[1]+(-4*t**2+4*t)*p3[1]+(2*t**2-t)*p4[1]
-        draw_point((x, y))
-    draw_point(p4)
+    #for i in range(50, 100, 2):
+    #    t = i / 100
+    #    x = (2*t**2-3*t+1)*p2[0]+(-4*t**2+4*t)*p3[0]+(2*t**2-t)*p4[0]
+    #    y = (2*t**2-3*t+1)*p2[1]+(-4*t**2+4*t)*p3[1]+(2*t**2-t)*p4[1]
+    #    draw_point((x, y))
+    #draw_point(p4)
 
 prepare_turtle_canvas()
 
+points = [(-300, 200), (400, 350), (300, -300), (-250, -200)]
+cnt = 1
+
 while True:
-    draw_curve_4_points((-350, -100), (-50, 200), (150, -100), (350, 300))
+    draw_curve_4_points(points[cnt-3], points[cnt-2], points[cnt-1], points[cnt])
+    cnt = (cnt + 1) % 4
 
 turtle.done()
