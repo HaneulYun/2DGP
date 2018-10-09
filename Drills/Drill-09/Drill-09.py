8bd7236a888f740abf6c6afe79775aac9a84644f
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-10-09 18:54:59 +0900

    update ball balacing
    update ball balacing

--- Drills/Drill-09/Drill-09.py
+++ Drills/Drill-09/Drill-09.py
@@ -22,16 +22,16 @@ class Boy:
 class Ball:
     def __init__(self):
         self.x, self.y = random.randint(0, 800), 599
-        self.speed = random.randint(2, 10)
+        self.speed = random.randint(5, 20)
         if(random.randint(0, 1)):
             self.image = load_image('ball21x21.png')
         else:
             self.image = load_image('ball41x41.png')
     def update(self):
-        if(self.y > 90):
+        if(self.y + self.image.h / 2 > 90 - 40):
             self.y -= self.speed
-            if(self.y < 90):
-                self.y = 90
+            if(self.y < 90 - 40 + self.image.h / 2):
+                self.y = 90 - 40 + self.image.h / 2
     def draw(self):
         self.image.draw(self.x, self.y)
 
