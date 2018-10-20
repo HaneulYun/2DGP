648f7525b2a8ec3f3e2fa03657f2c7ba70928576
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-10-20 23:41:59 +0900

    term project : stage background image
    term project : stage background image

--- project/main_state.py
+++ project/main_state.py
@@ -14,10 +14,12 @@ font = None
 
 class Stage:
     def __init__(self):
-        self.image = load_image('resources\sprites\Rounds\Misc Backgrounds.png')
+        self.image = load_image('resources\sprites\Rounds\Round A-1.png')
 
     def draw(self):
-        self.image.clip_draw(5, 2768 - 223, 320, 208, 320 * game_framework.windowScale // 2, 208 * game_framework.windowScale // 2, 320 * game_framework.windowScale, 208 * game_framework.windowScale)
+        self.image.clip_draw(320 * 3, 208 * 6, 352, 224, 320 * game_framework.windowScale // 2, 240 * game_framework.windowScale // 2, 320 * game_framework.windowScale, 240 * game_framework.windowScale)
+        self.image.clip_draw(320 * 0, 208 * 6, 320, 208, 320 * game_framework.windowScale // 2, 240 * game_framework.windowScale // 2, 320 * game_framework.windowScale, 240 * game_framework.windowScale)
+        self.image.clip_draw(320 * 0, 208 * 5, 320, 208, 320 * game_framework.windowScale // 2, 208 * game_framework.windowScale // 2, 320 * game_framework.windowScale, 208 * game_framework.windowScale)
 
 
 MOTION_TYPE = 0
