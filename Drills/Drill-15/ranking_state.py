e71557a7b2bcf0c87ef18dc7c5c63cd2da0f3568
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-11-27 18:57:03 +0900

    complete colision
    complete colision

--- Drills/Drill-15/ranking_state.py
+++ Drills/Drill-15/ranking_state.py
@@ -8,31 +8,11 @@ import game_framework
 import game_world
 
 import world_build_state
-import ranking_state
 
-import zombie
+name = "RankingState"
 
-name = "MainState"
-
-
-def collide(a, b):
-    # fill here
-    left_a, bottom_a, right_a, top_a = a.get_bb()
-    left_b, bottom_b, right_b, top_b = b.get_bb()
-
-    if left_a > right_b: return False
-    if right_a < left_b: return False
-    if top_a < bottom_b: return False
-    if bottom_a > top_b: return False
-
-    return True
-
-boy = None
 
 def enter():
-    # game world is prepared already in world_build_state
-    global boy
-    boy = world_build_state.get_boy()
     pass
 
 def exit():
@@ -62,11 +42,6 @@ def handle_events():
 def update():
     for game_object in game_world.all_objects():
         game_object.update()
-        if isinstance(game_object, zombie.Zombie):
-            if collide(game_object, boy):
-                game_framework.change_state(ranking_state)
-
-
 
 
 def draw():
--- Drills/Drill-15/zombie.py
+++ Drills/Drill-15/zombie.py
@@ -100,6 +100,7 @@ class Zombie:
         self.x = clamp(50, self.x, get_canvas_width() - 50)
         self.y = clamp(50, self.y, get_canvas_height() - 50)
 
+
     def draw(self):
         tw, th = int(100*self.size), int(100*self.size)
         if math.cos(self.dir) < 0:
